d03c89fc6fc0376e6a6dfecfd0192ff48b435611
jk <jk9958@gmail.com>
2025-04-24 21:28:04 +0300

    Initial Commit

---
+++ ConstructionManagerApp/ConstructionManagerAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  ConstructionManagerAppApp.swift
+//  ConstructionManagerApp
+//
+//  Created by DakshinAJK on 24/04/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ConstructionManagerAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ConstructionManagerAppTests/ConstructionManagerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConstructionManagerAppTests.swift
+//  ConstructionManagerAppTests
+//
+//  Created by DakshinAJK on 24/04/2025.
+//
+
+import Testing
+@testable import ConstructionManagerApp
+
+struct ConstructionManagerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
